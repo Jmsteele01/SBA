881174076c973c0661ee85b51fe792989fbd7c84
Jmsteele01 <Jmsteele01@hotmail.com>
2025-04-12 20:06:55 -0600

    basic search functionality

--- src/script.mjs
+++ src/script.mjs
@@ -2,24 +2,35 @@ const getEmote = await fetch("https://emojihub.yurace.pro/api/random");
 const newEmote = await getEmote.json();
 const getAllEmotes = await fetch("https://emojihub.yurace.pro/api/all");
 const allEmotes = await getAllEmotes.json();
-
-console.log("Fuck");
+const randomEmote = newEmote.htmlCode[0];
 
 const opening = document.body.querySelector(".opening");
+const results = document.body.querySelector(".results");
 let title = document.createElement("h1");
 opening.appendChild(title);
-title.textContent = "Foobar"
+title.innerHTML = `${randomEmote} Emoji World!!! ${randomEmote}`
 
-// let showResults = document.createElement("p")
-
-// opening.textContent = "This is a search thing."
+allEmotes.forEach(emote => {
+    const ul = document.createElement("ul");
+    ul.innerHTML = emote.htmlCode[0]
+    results.appendChild(ul);
+});
 
 document.getElementById("search_by_name_btn").addEventListener("click", search);
 
 function search()
 {
-    let currentSearch = document.getElementById("search_bar")
+    const currentSearch = document.getElementById("search_bar")
     console.log("HI!");
-
+    results.textContent = "";
+    allEmotes.forEach(emote => {
+        let foundEmote = emote.name.toLowerCase().includes(currentSearch.value.toLowerCase())
+        if(foundEmote)
+        {
+            const ul = document.createElement("ul");
+            ul.innerHTML = emote.htmlCode[0]
+            results.appendChild(ul);
+        }
+    });
 }
 
